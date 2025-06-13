6ecbf456d9d30036dbc8666ea7c899d5ecd9bf5c
Glenn Torrens <glenntorrens@gmail.com>
2025-06-13 16:09:03 +0900

    added font to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,11 +27,14 @@ export default function Home() {
 
       <audio ref={audioRef} loop src="/rick and morty.mp3" />
       <div className="relative z-10 flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-4xl m-0 backdrop-blur-md bg-white/30 p-6 rounded-2xl ripple shadow-lg">
-          Welcome to the Rick and Morty API
-        </h1>
+       
 
-        
+        <Image
+         src="/font.png"
+         alt="font"
+         width={700}
+         height={700}
+         className=" m-0 backdrop-blur-md bg-black/40 p-4 rounded-2xl ripple shadow-lg" />
 
         <div className="mt-10">
           <Navbar />
