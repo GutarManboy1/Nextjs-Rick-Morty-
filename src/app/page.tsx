6da0e1c677bcb77cb147b03949d877bf3ad2b948
Glenn Torrens <glenntorrens@gmail.com>
2025-06-13 15:54:07 +0900

    added musical plumbus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
+"use client";
 import { Navbar } from "@/components/navbar";
 import Image from "next/image";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const toggleAudio = () => {
+    if (!audioRef.current) return;
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
   return (
-    <main className="relative min-h-screen">
+    <main className="relative min-h-screen overflow-hidden">
       <Image
         src="/portal.webp"
         alt="portal"
         fill
-        className="object-cover filter blur z-0"
+        className="object-contain rotate-slow filter blur z-0"
       />
 
+      <audio ref={audioRef} loop src="/rick and morty.mp3" />
       <div className="relative z-10 flex flex-col items-center justify-center min-h-screen p-4">
         <h1 className="text-4xl m-0 backdrop-blur-md bg-white/30 p-6 rounded-2xl ripple shadow-lg">
           Welcome to the Rick and Morty API
         </h1>
+
+        
+
         <div className="mt-10">
           <Navbar />
         </div>
+
+        <Image
+          src={isPlaying ? "/plumbus.png" : "/plumbus.png"}
+          alt="Play or pause audio"
+          width={100}
+          height={100}
+          onClick={toggleAudio}
+          className="absolute bottom-12 left-1 cursor-pointer transform transition duration-500 ease-in-out hover:scale-110"
+        />
+          
       </div>
     </main>
   );
