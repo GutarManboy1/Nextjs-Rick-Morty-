f99b1646c04ed04297b51a4fae787aa23815f48b
Glenn Torrens <glenntorrens@gmail.com>
2025-06-13 14:52:15 +0900

    added background image to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import { Navbar } from "@/components/navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-4xl m-0">Welcome to the Rick and Morty API</h1>
-      <div className="mt-10">
-        <Navbar />
+    <main className="relative min-h-screen">
+      <Image
+        src="/portal.webp"
+        alt="portal"
+        fill
+        className="object-cover filter blur z-0"
+      />
+
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen p-4">
+        <h1 className="text-4xl m-0 backdrop-blur-md bg-white/30 p-6 rounded-2xl ripple shadow-lg">
+          Welcome to the Rick and Morty API
+        </h1>
+        <div className="mt-10">
+          <Navbar />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
